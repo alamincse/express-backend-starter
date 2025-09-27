7004031b793e232cd7469217ca09da46eb452c08
Al-Amin Sarker <cseal.amin.09@gmail.com>
2025-09-27 17:24:51 +0600

    feat: add redirect authenticated middleware

---
+++ app/middleware/RedirectIfAuthenticated.js
@@ -0,0 +1,47 @@
+const jwt = require('jsonwebtoken');
+
+class RedirectIfAuthenticated {
+	handle = (req, res, next) => {
+        try {
+            let token = '';
+            let isBearer = false;
+
+            const authHeader = req.headers['authorization'] ?? '';
+
+            // Check Bearer token
+            if (authHeader && authHeader.startsWith('Bearer ')) {
+                token = authHeader.split(' ')[1];
+
+                isBearer = true;
+            } else if (req.cookies && req.cookies.session_token) {
+            	// Check cookie token
+                token = req.cookies.session_token;
+            }
+
+            if (! token) {
+                // no token, proceed to login/register page
+                return next();
+            }
+
+            // JWT verify
+            try {
+	            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+	            return res.redirect('/dashboard'); // token valid, redirect to dashboard
+	        } catch (err) {
+	            console.log(err);
+
+	            // proceed to login/register page
+	            return next();
+	        }
+
+        } catch (err) {
+            console.error(err.stack ?? err.message);
+
+            // proceed to login/register page
+            next();
+        }
+    }
+}
+
+module.exports = new RedirectIfAuthenticated();
--- routes/web.js
+++ routes/web.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const Route = express.Router();
 
+const RedirectIfAuthenticated = require('@app/middleware/RedirectIfAuthenticated');
 const DashboardController = require('@app/controllers/web/DashboardController');
 const HomeController = require('@app/controllers/web/HomeController');
 const AuthMiddleware = require('@app/middleware/AuthMiddleware');
 
-Route.get('/', (req, res) => HomeController.index(req, res));
-Route.get('/register', (req, res) => HomeController.register(req, res));
+Route.get('/', 
+	(req, res, next) => RedirectIfAuthenticated.handle(req, res, next),
+	(req, res) => HomeController.index(req, res)
+);
+
+Route.get('/register', 
+	(req, res, next) => RedirectIfAuthenticated.handle(req, res, next),
+	(req, res) => HomeController.register(req, res)
+);
 
 Route.get('/dashboard', 
 	(req, res, next) => AuthMiddleware.handle(req, res, next), 
