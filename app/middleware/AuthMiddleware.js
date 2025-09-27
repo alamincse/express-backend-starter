8093e6f3da32463989c0a4dbd7644bf190a135b1
Al-Amin Sarker <cseal.amin.09@gmail.com>
2025-09-27 16:51:41 +0600

    feat: add auth middleware

---
+++ app/middleware/AuthMiddleware.js
@@ -0,0 +1,58 @@
+const jwt = require('jsonwebtoken');
+
+class AuthMiddleware {
+	handle = (req, res, next) => {
+		try {
+			let token = '';
+			let isBearer = false;
+
+			const authHeader = req.headers['authorization']
+
+			// check bearer token
+			if (authHeader && authHeader.startsWith('Bearer ')) {
+			    token = authHeader.split(' ')[1];
+
+			    isBearer = true;
+			} else if (req.cookies && req.cookies.session_token) { 
+				// check cookie token
+			    token = req.cookies.session_token;
+			}
+
+	        if (! token) {
+	        	// redirect if cookie, else JSON response
+	            if (isBearer) {
+	                return res.status(401).json({ message: 'Unauthorized: No token provided' });
+	            } else {
+	                return res.redirect('/');
+	            }	            
+	        }
+
+	        // JWT verification (async)
+            let payload;
+
+            try {
+                payload = jwt.verify(token, process.env.JWT_SECRET);
+            } catch (err) {
+                if (isBearer) {
+                	return res.status(403).json({ message: 'Forbidden: Invalid or expired token' });
+                } else {
+                	return res.redirect('/');
+                }
+            }
+
+	        req.user = payload;
+
+	        next();
+		} catch (err) {
+			Log.error(err.stack ?? err.message);
+
+			if (req?.headers['authorization']) {
+	            return res.status(500).json({ error: err.message });
+	        } else {
+	            return res.redirect('/');
+	        }
+		}
+	}
+}
+
+module.exports = new AuthMiddleware();
--- routes/web.js
+++ routes/web.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const Route = express.Router();
 
-const HomeController = require('@app/controllers/web/HomeController');
 const DashboardController = require('@app/controllers/web/DashboardController');
+const HomeController = require('@app/controllers/web/HomeController');
+const AuthMiddleware = require('@app/middleware/AuthMiddleware');
 
 Route.get('/', (req, res) => HomeController.index(req, res));
 Route.get('/register', (req, res) => HomeController.register(req, res));
 
-Route.get('/dashboard', (req, res) => DashboardController.index(req, res));
+Route.get('/dashboard', 
+	(req, res, next) => AuthMiddleware.handle(req, res, next), 
+	(req, res) => DashboardController.index(req, res)
+);
 
 module.exports = Route;
