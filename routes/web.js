c9e8f20fdc46bd2d825b8f5af28ac60916516fa5
Al-Amin Sarker <cseal.amin.09@gmail.com>
2025-09-27 08:01:57 +0600

    feat: view routes for web

--- routes/web.js
+++ routes/web.js
@@ -1,4 +1,9 @@
-// const express = require('express');
-// const Route = express.Router();
+const express = require('express');
+const Route = express.Router();
 
-// module.exports = Route;
+const HomeController = require('@app/controllers/web/HomeController');
+
+Route.get('/', (req, res) => HomeController.index(req, res))
+Route.get('/register', (req, res) => HomeController.register(req, res))
+
+module.exports = Route;
