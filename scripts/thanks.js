7f5b74286ec9774dd402f60c5984c93491407727
Al-Amin Sarker <cseal.amin.09@gmail.com>
2025-09-28 21:22:53 +0600

    feat: update thank you

--- scripts/thanks.js
+++ scripts/thanks.js
@@ -1,8 +1,8 @@
 console.log(`
---------------------------------------------------
+----------------------------------------------------------------
         Thanks for installing Express Backend Starter 🙏
   Please consider giving a ⭐ on GitHub to support
 
     👉 https://github.com/alamincse/express-backend-starter
---------------------------------------------------
+----------------------------------------------------------------
 `);
